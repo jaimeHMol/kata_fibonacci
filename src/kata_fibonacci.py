c4725b72ac427e9aac49a55da8ea8df98e9e5eaf
Jaime Herran <jaimehmol@yahoo.com>
2024-01-13 01:06:46 -0500

    Add docstrings.

--- src/kata_fibonacci.py
+++ src/kata_fibonacci.py
@@ -1,12 +1,15 @@
 
 def fibonacci(num_fibonacci:int = 10) -> list:
+    """ Generates the Fibonacci series of numbers up to the num_fibonacci received as
+    input. """
+
+    # Validate inputs
     if num_fibonacci <= 0 or not type(num_fibonacci) == int:
         raise ValueError("The Fibonacci number must be a positive integer greater than 0.")
 
     num1 = 0
     num2 = 1
     fibonacci_series = []
-
     fibonacci_series.append(num1)
     fibonacci_series.append(num2)
     for i in range(num_fibonacci - 2):
